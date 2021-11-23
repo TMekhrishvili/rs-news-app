b0bdfa001c3e55898520381d1e2d5a983e1d2825
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-11-23 23:58:21 +0400

    feat: part of card

--- src/features/home/components/card/Card.jsx
+++ src/features/home/components/card/Card.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import style from './Card.module.css';
+import { Link } from 'react-router-dom';
 
 const Card = ({ data }) => {
+    console.log(data)
     return (
-        <div>
+        <div className={style.cardContainer}>
+            <div className={style.imageContainer}>
+                <img src={data.urlToImage} alt={data.title} className={style.image} />
+            </div>
+            <div className={style.articleContainer}>
+                <h3 className={style.articleTitle} >{data.title}</h3>
+                <p>{data.description}</p>
+                <Link className={style.readMore} to="#">Read More</Link>
+            </div>
 
+            {/* Source, Date of publication*/}
         </div>
     )
 }
