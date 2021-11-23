54fff69abff88f61b48244e1d8c423641db0486c
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-11-23 22:59:44 +0400

    feat: axios and environment variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_API_KEY}`;
 
 ReactDOM.render(
   <App />,
