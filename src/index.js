fc69f825ae80ca032ee85d91f1d63d3988efb4bf
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-11-23 23:14:39 +0400

    fix: env

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_API_KEY}`;
+axios.defaults.baseURL = 'https://newsapi.org/' //process.env.REACT_APP_API_URL;
+axios.defaults.headers.common['Authorization'] = `Bearer 252f1ab95e8445d7a107b8eca39a02be`//${process.env.REACT_APP_API_KEY}`;
 
 ReactDOM.render(
   <App />,
