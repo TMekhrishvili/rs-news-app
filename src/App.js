a9f3b9f18589b538f3f04174aae25fb5653dae17
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-11-23 23:09:16 +0400

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './features/home/Home';
+import Article from './features/article/Article';
+import Page404 from './features/page404/Page404';
 
 const App = () => {
     return (
-        <div>
-            app
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="article/:id" element={<Article />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/features/page404/Page404.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Page404 = () => <p>404: page not found</p>
+
+export default Page404
