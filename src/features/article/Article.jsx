e992525ad0dcd2fa6ce76ad483908c17df238253
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-12-02 19:42:02 +0400

    feat: stop loader in case of error

--- src/features/article/Article.jsx
+++ src/features/article/Article.jsx
@@ -9,31 +9,36 @@ import news from '../../assets/images/news.jpg';
 const Article = () => {
     let params = useParams();
     const [article, setArticle] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const fetchArticle = async () => {
             const result = await fetchArticles(params.id);
             setArticle(result.data.articles[0]);
+            setLoading(false);
         }
         fetchArticle();
     }, [params.id])
 
     return (
         <div>
-            {article ?
-                <div className={style.container}>
-                    <h1 className={style.title}>{article.title}</h1>
-                    <div className={style.imgContainer}>
-                        <img className={style.img} src={article.urlToImage === '' ? news : article.urlToImage} alt={article.title} />
-                    </div>
-                    <p className={style.content} dangerouslySetInnerHTML={{ __html: article.content }} ></p>
-                    <div className={style.articleFooter}>
-                        <p><span className={style.bold}>Author: </span>{article.author}</p>
-                        <p><span className={style.bold}>Published: </span>{moment(article.publishedAt).format('DD MMM YYYY HH:mm')}</p>
-                    </div>
-                </div>
-                :
-                <Loader />}
+            {loading ?
+                <Loader /> :
+                article ?
+                    <div className={style.container}>
+                        <h1 className={style.title}>{article.title}</h1>
+                        <div className={style.imgContainer}>
+                            <img className={style.img} src={article.urlToImage === '' ? news : article.urlToImage} alt={article.title} />
+                        </div>
+                        <p className={style.content} dangerouslySetInnerHTML={{ __html: article.content }} ></p>
+                        <div className={style.articleFooter}>
+                            <p><span className={style.bold}>Author: </span>{article.author}</p>
+                            <p><span className={style.bold}>Published: </span>{moment(article.publishedAt).format('DD MMM YYYY HH:mm')}</p>
+                        </div>
+                    </div> :
+                    <p className={style.smthWrong}>Something went wrong!</p>
+            }
         </div>
     )
 }
